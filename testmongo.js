5931cb02c259227b8c5e9d40267ee9d6394fa498
claretd23 <mariaclaretdelgadomacias@gmail.com>
2025-10-24 00:04:01 -0600

    Corrigido formato del archivo .env

---
+++ testmongo.js
@@ -0,0 +1,7 @@
+// testMongo.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Conexión a MongoDB exitosa ✅'))
+  .catch(err => console.error('Error de conexión:', err));
